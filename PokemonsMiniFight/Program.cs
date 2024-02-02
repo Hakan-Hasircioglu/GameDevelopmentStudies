6afb10831a9b92f952359cce1ae7ba25ebbfe01a
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2024-02-02 03:12:17 +0300

    Version 0.002

--- PokemonsMiniFight/Program.cs
+++ PokemonsMiniFight/Program.cs
@@ -67,9 +67,107 @@ namespace Pokemons_Mini_Fight
             Console.WriteLine();
             #endregion
             #region Fight
-            for (int i = 1; i <= 3; i++)
+            #region Assign Attack Stat
+            if (ChooseOne == Charmender)
             {
-                #region Assign Attack Stat
+                ChoosenAttack = AttackGenerator.Next(22, 32);
+            }
+            else if (ChooseOne == Squirtle)
+            {
+                ChoosenAttack = AttackGenerator.Next(18, 28);
+            }
+            else if (ChooseOne == Balbasaur)
+            {
+                ChoosenAttack = AttackGenerator.Next(19, 29);
+            }
+            if (ChooseEnemy == Charmender)
+            {
+                EnemyAttack = AttackGenerator.Next(22, 32);
+            }
+            else if (ChooseEnemy == Squirtle)
+            {
+                EnemyAttack = AttackGenerator.Next(18, 28);
+            }
+            else if (ChooseEnemy == Balbasaur)
+            {
+                EnemyAttack = AttackGenerator.Next(19, 29);
+            }
+            #endregion 
+            for (int i = 1; i <= 5; i++)
+            {
+                #region Conclusion
+                if (ChoosenHealth <= 0 && EnemyHealth <= 0)
+                {
+                    Console.WriteLine($"{ChooseOne}:0hp\n" +
+                                        $"{ChooseEnemy}:0hp");
+                    Console.WriteLine("Draw!!!");
+                    break;
+                }
+                else if (EnemyHealth <= 0 && ChoosenHealth > 0)
+                {
+                    Console.WriteLine($"{ChooseOne} Win!!!");
+                    Console.WriteLine($"{ChooseOne}{ChoosenHealth}hp\n" +
+                        $"{ChooseEnemy}: 0hp");
+                    break;
+                }
+                else if (ChoosenHealth <= 0 && EnemyHealth > 0)
+                {
+                    Console.WriteLine($"{ChooseEnemy} Win!!!");
+                    Console.WriteLine($"{ChooseEnemy}:0hp\n" +
+                                        $"{ChooseEnemy}:0hp");
+                    Console.WriteLine("Draw!!!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{ChooseOne}:{ChoosenHealth}hp\n" +
+                                        $"{ChooseEnemy}:{EnemyHealth}hp");
+                }
+                #endregion
+                EnemyAttack -= AttackGenerator.Next(1,10);
+                ChoosenAttack -= AttackGenerator.Next(1, 10);
+                #region Momentary Ones
+                Console.WriteLine();
+                Console.WriteLine($"Round {i}");
+                Console.WriteLine(new string ('-', 50));
+                Console.WriteLine("Choose your move:\n" +
+                    "1.\t Strong Attack\n" +
+                    "2.\t Healing\n" +
+                    "3.\t Normal Attack");
+                Console.WriteLine(new string('-', 50));
+                string move = Console.ReadLine();
+                Console.WriteLine(new string('-', 50));
+                if (move == "1")
+                {
+                    ChoosenAttack += 10;
+                }
+                else if (move == "2")
+                {
+                    ChoosenAttack = 0;
+                    ChoosenHealth += AttackGenerator.Next(20, 30);
+                }
+                #region Result
+                
+                EnemyHealth -= ChoosenAttack;
+
+                ChoosenHealth -= EnemyAttack;
+                #endregion
+
+                Console.WriteLine($"{ChooseOne} Attacks {ChooseEnemy} and deals {ChoosenAttack} damage ");
+                Console.WriteLine($"{ChooseEnemy} Attacks {ChooseOne} and deals {EnemyAttack} damage ");
+                Console.WriteLine();
+                #endregion
+                
+                #region Alternative Result
+                if (i == 5)
+                {
+                    Console.WriteLine($"{ChooseOne}:{ChoosenHealth}hp\n" +
+                                        $"{ChooseEnemy}:{EnemyHealth}hp");
+                    Console.WriteLine();
+                    Console.WriteLine("Draw!!!");
+                }
+                #endregion
+                #region Again Attack Stats
                 if (ChooseOne == Charmender)
                 {
                     ChoosenAttack = AttackGenerator.Next(22, 32);
@@ -93,52 +191,6 @@ namespace Pokemons_Mini_Fight
                 else if (ChooseEnemy == Balbasaur)
                 {
                     EnemyAttack = AttackGenerator.Next(19, 29);
-                }
-                #endregion                                
-                #region Result
-                if (ChoosenHealth <= 0 && EnemyHealth <= 0)
-                {
-                    Console.WriteLine($"{ChooseOne}:0hp\n" +
-                                        $"{ChooseEnemy}:0hp");
-                    Console.WriteLine("Draw!!!");
-                    break;
-                }
-                else if (EnemyHealth <= 0 && ChoosenHealth > 0)
-                {
-                    Console.WriteLine($"{ChooseOne} Win!!!");
-                    Console.WriteLine($"{ChooseOne}{ChoosenHealth}hp\n" +
-                        $"{ChooseEnemy}: 0hp");
-                    break;
-                }               
-                else if (ChoosenHealth <= 0 && EnemyHealth > 0)
-                {
-                    Console.WriteLine($"{ChooseEnemy} Win!!!");
-                    Console.WriteLine($"{ChooseEnemy}:0hp\n" +
-                                        $"{ChooseEnemy}:0hp");
-                    Console.WriteLine("Draw!!!");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"{ChooseOne}:{ChoosenHealth}hp\n" +
-                                        $"{ChooseEnemy}:{EnemyHealth}hp");
-                }
-                EnemyHealth -= ChoosenAttack;
-
-                ChoosenHealth -= EnemyAttack;
-                #endregion
-                #region Momentary Ones
-                Console.WriteLine();
-                Console.WriteLine($"Round {i}");
-                Console.WriteLine(new string ('-', 50));
-                Console.WriteLine($"{ChooseOne} Attacks {ChooseEnemy} and deals {ChoosenAttack} damage ");
-                Console.WriteLine($"{ChooseEnemy} Attacks {ChooseOne} and deals {EnemyAttack} damage ");
-                Console.WriteLine();
-                #endregion
-                #region Alternative Result
-                if (i == 3)
-                {
-                    Console.WriteLine("Draw!!!");
                 }
                 #endregion
             }
