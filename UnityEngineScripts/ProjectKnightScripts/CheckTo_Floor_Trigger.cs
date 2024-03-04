a90cf0a3ded25ca533f58b2a77c225d0e84088be
Hakan Hasircioglu <hasircioglu.hakan02@gmail.com>
2024-03-05 00:16:13 +0300

    Only One .cs File

---
+++ UnityEngineScripts/ProjectKnightScripts/CheckTo_Floor_Trigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CheckToFloorTrigger : MonoBehaviour
+{
+    #region Variables
+    public GameObject playerObject;
+    bool isContactToFloor;
+    #endregion
+    
+    //! Check the player contact !
+    //? Does the player have any contact with the ground ?
+    //* If the player hasn't any contact with the ground, the player cannot jump. *
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Parkour"))
+        {
+            PlayerMove playerMove = playerObject.GetComponent<PlayerMove>();
+            playerMove.jumpJump = true;
+        }
+    }
+    void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Parkour"))
+        {
+            PlayerMove playerMove = playerObject.GetComponent<PlayerMove>();
+            playerMove.jumpJump = false;
+        }
+    }
+}
